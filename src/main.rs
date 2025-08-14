b9f1f3b4d236c22d5d45f27016eb23a9233b895e
cqn-brwpna9 <im.a.nony.moose.o@gmail.com>
2025-08-13 18:06:07 -0700

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![allow(unused)]
+use text_io::read as readin;
+
+fn read() -> String{
+    print!(">");
+    let input=readin!("{}\n");
+    return input;
+}
+
+fn eval(ast: String)->String{
+	println!("in eval, got {ast}");
+	return ast;
+}
+
+fn print(string: String){
+	println!("{string}");
+}
+
+
+fn main() {
+    loop{
+       print(eval(read()));
+    }
+}
+
